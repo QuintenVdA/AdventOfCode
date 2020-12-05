48ac4bcb9f3502f68953809006ea3649c8c451a1
QuintenVdA <quinten.vanderauwera@gmail.com>
2020-12-05 17:13:34 +0100

    Add shorter solution for day 5.

---
+++ 2020/5/solution2.py
@@ -0,0 +1,23 @@
+import re
+
+result1, result2, allSeats = 0, 0, []
+
+input = open('input', 'r')
+
+for line in input:
+    seat = int(re.sub('(B|R)', '1', re.sub('(F|L)', '0', line)), 2)
+
+    allSeats.append(seat)
+
+    if seat > result1:
+        result1 = seat
+
+input.close()
+
+result2 = result1
+
+while result2 in allSeats:
+    result2 -= 1
+
+print('Part 1: ' + str(result1))
+print('Part 2: ' + str(result2))
